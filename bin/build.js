932fe045b6f01019f4651191f800fb639747895e
Martijn Swart <martijn@studiokloek.nl>
2022-06-22 11:53:16 +0200

    updated build

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,33 @@
 #!/usr/bin/env node
+import meow from 'meow';
 import {kloekBuild} from '../dist/index.js';
 
-await kloekBuild();
+const cli = meow(
+  `
+	Usage
+	  $ kloek-build 
+
+    Options:
+      --path PATH           set the base path
+
+    Other options:
+      -e, --empty-out-dir   empty output dir before building
+`,
+  {
+    importMeta: import.meta,
+    flags: {
+      emptyOutDir: {
+        type: 'boolean',
+        alias: 'e',
+        default: true,
+      },
+      base: {
+        type: 'string',
+        alias: 'b',
+        default: '/',
+      },
+    },
+  },
+);
+
+await kloekBuild(cli.flags);
