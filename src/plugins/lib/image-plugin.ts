73c44958ddfe7bdb4712cf032e150e7539ac607f
Martijn Swart <martijn@studiokloek.nl>
2023-11-16 14:25:07 +0100

    [images] Caching disabled temporary

--- src/plugins/lib/image-plugin.ts
+++ src/plugins/lib/image-plugin.ts
@@ -1,21 +1,18 @@
-
 import path from 'node:path';
-import {cwd} from '../../utils';
 import type {ConfigEnv, Plugin} from 'vite';
-import type {ImageQualityConfig} from '../../utils/interfaces'; 
-
-import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
 
 // The minifiers you want to use:
-import imageminMozjpeg from 'imagemin-mozjpeg'
-import imageminPngQuant from 'imagemin-pngquant'
-import imageminOptiPng from 'imagemin-optipng'
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminOptiPng from 'imagemin-optipng';
+import type {ImageQualityConfig} from '../../utils/interfaces';
+import {cwd} from '../../utils';
 
 export function imageminPlugin(
   environment: ConfigEnv,
   options: ImageQualityConfig,
 ): Plugin | undefined {
-
   if (environment.mode !== 'production') {
     return;
   }
@@ -23,12 +20,13 @@ export function imageminPlugin(
   return viteImagemin({
     skipIfLarger: true,
     verbose: true,
+    cache: false,
     root: path.join(path.resolve(cwd), 'public'),
     plugins: {
       jpg: [
         imageminMozjpeg({
           quality: options?.jpeg ?? 80,
-        })
+        }),
       ],
       png: [
         imageminPngQuant({
@@ -36,9 +34,9 @@ export function imageminPlugin(
           speed: 2,
           dithering: 0.25,
           quality: options?.png ?? [0.5, 0.7],
-        }), 
-        imageminOptiPng()
-      ]
-    }
+        }),
+        imageminOptiPng(),
+      ],
+    },
   }) as Plugin;
-};
+}
